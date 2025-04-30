273bc2a4e49ffe4d44ad2bccec62dd68ffdda1c7
Anguinao, Leonardo <languiano@csus.edu>
2025-04-30 15:50:55 -0700

    display the critic score

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -2,9 +2,12 @@ import { useEffect, useState } from "react";
 import apiClient from "../services/api-client";
 import { CanceledError } from "axios";
 
-interface Game {
+export interface Game {
   id: number;
   name: string;
+  background_image: string;
+  parent_platforms: {platform: Platform}[];
+    metacritic: number;
 }
 
 export interface Platform {
@@ -16,8 +19,6 @@ export interface Platform {
 export interface FetchGamesResponse {
   count: number;
   results: Game[];
-  background_image: string;
-  parent_platforms: {platform: Platform}[];
     
 }
 
