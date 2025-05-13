4db9e82226d077dab972998a2a7885a0ded86c0a
Anguinao, Leonardo <languiano@csus.edu>
2025-05-13 16:19:21 -0700

    refactor for deployment

--- src/components/GameCardSkeleton.tsx
+++ src/components/GameCardSkeleton.tsx
@@ -1,10 +1,9 @@
 import { Card, Skeleton, SkeletonText, CardBody } from "@chakra-ui/react";
-import React, { ReactNode } from "react";
 
 const GameCardSkeleton = () => {
   return (
     <div>
-      <Card >
+      <Card>
         <Skeleton height="200px" />
         <CardBody>
           <SkeletonText />
